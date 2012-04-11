d398bb830c2e26df936dba8d7af9d097c74c0472
Cosmin <cosminstefanxp@gmail.com>
2012-04-11 15:08:31 +0300

    First real commit. Logging functional.

--- src/com/fenceit/FenceItActivity.java
+++ src/com/fenceit/FenceItActivity.java
@@ -1,13 +1,38 @@
 package com.fenceit;
 
+import org.apache.log4j.Logger;
+
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * The Class FenceItActivity.
+ */
 public class FenceItActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	/** The logger. */
+	private static Logger log = Logger.getRootLogger();
+
+	/**
+	 * Called when the activity is first created.
+	 * 
+	 * @param savedInstanceState
+	 *            the saved instance state
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		init();
+		setContentView(R.layout.main);
+	}
+
+	/**
+	 * Initializes the application environment.
+	 */
+	private void init() {
+		//DOMConfigurator.configure("log4j.xml");
+//		BasicConfigurator.configure();
+		new Log4jConfiguration();
+		log.info("Starting up...");
+	}
 }
---
+++ src/com/fenceit/Log4jConfiguration.java
@@ -0,0 +1,36 @@
+package com.fenceit;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.log4j.Level;
+
+import android.os.Environment;
+import de.mindpipe.android.logging.log4j.LogConfigurator;
+
+/**
+ * Configures the Log4J Environment. 
+ * 
+ * NOTE: To check the files on the SD, either user Eclipse -> DDMS -> File Explorer
+ * or use "$android-sdks/platform-tools/adb shell"
+ */
+public class Log4jConfiguration {
+	static {
+		final LogConfigurator logConfigurator = new LogConfigurator();
+		//File log configuration
+		logConfigurator.setUseFileAppender(true);
+		logConfigurator.setFileName(Environment.getExternalStorageDirectory()
+				+ "/FenceIt/Main.log");
+		logConfigurator.setMaxFileSize(1024*50);
+		logConfigurator.setMaxBackupSize(3);
+		logConfigurator.setFilePattern("%d [%-5p] %c - %m%n");
+		
+		//Log cat configuration
+		logConfigurator.setUseLogCatAppender(true);
+		logConfigurator.setLogCatPattern("[%-5p] %c - %m%n");
+
+		//Basic configuration
+		logConfigurator.setRootLevel(Level.DEBUG);
+		logConfigurator.configure();
+	}
+}
