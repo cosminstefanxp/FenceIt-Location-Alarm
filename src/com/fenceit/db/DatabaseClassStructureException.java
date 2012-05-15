cdbf19507ea4f379531d1d7d6adcd239823b312d
Cosmin <cosminstefanxp@gmail.com>
2012-05-16 01:07:17 +0300

    Started work on restructured Database Tools. Finished Reflection Manager.

--- src/com/fenceit/db/DatabaseClassStructureException.java
+++ src/com/fenceit/db/DatabaseClassStructureException.java
@@ -7,7 +7,9 @@
 package com.fenceit.db;
 
 /**
- * The Class DatabaseClassStructureException.
+ * The DatabaseClassStructureException is thrown when the structure of a class that has to be stored
+ * in the database is not according to the specifications. Please check {@link ReflectionManager} and
+ * {@link DefaultDAO} for specifications.
  */
 public class DatabaseClassStructureException extends Exception {
 
