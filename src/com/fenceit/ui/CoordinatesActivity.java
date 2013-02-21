6ab3e392247c0cf45347a8ce061fe31106dcf254
Cosmin Stefan <cosminstefanxp@gmail.com>
2013-02-21 17:26:00 +0100

    Converted CoordinatesActivity

--- src/com/fenceit/ui/CoordinatesActivity.java
+++ src/com/fenceit/ui/CoordinatesActivity.java
@@ -11,7 +11,6 @@ import java.text.NumberFormat;
 import java.util.Date;
 
 import org.androwrapee.db.DefaultDAO;
-import org.apache.log4j.Logger;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -22,14 +21,11 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.fenceit.R;
-import com.fenceit.alarm.locations.AbstractAlarmLocation;
 import com.fenceit.alarm.locations.CoordinatesLocation;
 import com.fenceit.db.DatabaseManager;
 import com.fenceit.provider.CoordinatesDataProvider;
@@ -38,13 +34,10 @@ import com.fenceit.ui.picker.NumberPickerDialog;
 import com.fenceit.ui.picker.NumberPickerDialog.OnNumberSetListener;
 
 /**
- * The Class CoordinatesActivity used for setting up Locations based on Coordinates.
+ * The Class CoordinatesActivity used for setting up {@link CoordinatesLocation}.
  */
-public class CoordinatesActivity extends AbstractLocationActivity implements OnClickListener,
-		CoordinatesLocationDataListener {
-
-	/** The logger. */
-	private static final Logger log = Logger.getLogger(CoordinatesActivity.class);
+public class CoordinatesActivity extends AbstractLocationActivity2<CoordinatesLocation> implements
+		OnClickListener, CoordinatesLocationDataListener {
 
 	/** The Constant DIALOG_ENABLE_NETWORK. */
 	private static final int DIALOG_ENABLE_LOCALIZATION = 0;
@@ -55,15 +48,6 @@ public class CoordinatesActivity extends AbstractLocationActivity implements OnC
 	/** The data access object. */
 	private DefaultDAO<CoordinatesLocation> dao = null;
 
-	/** The location. */
-	private CoordinatesLocation location;
-
-	/** If it's a new entity. */
-	private boolean newEntity;
-
-	/** The save button. */
-	private Button saveButton;
-
 	/** The refresh button. */
 	private ImageButton refreshButton;
 
@@ -73,6 +57,7 @@ public class CoordinatesActivity extends AbstractLocationActivity implements OnC
 	/** The Constant used for number format. */
 	private static final NumberFormat nf = new DecimalFormat("##.########");
 
+	/** The Constant REQ_MAP_SELECT. */
 	private static final int REQ_MAP_SELECT = 0;
 
 	/** The location provider. */
@@ -87,37 +72,8 @@ public class CoordinatesActivity extends AbstractLocationActivity implements OnC
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.coordinates_location);
-//		((TextView) findViewById(R.id.title_titleText)).setText("Edit Location");
-
-		// Prepare database connection
-		if (dao == null)
-			dao = DatabaseManager.getDAOInstance(getApplicationContext(), CoordinatesLocation.class,
-					CoordinatesLocation.tableName);
-
-		// If it's a new activity
-		if (savedInstanceState == null) {
-			// Get the location from the database
-			Bundle extras = getIntent().getExtras();
-			Long locationID = (Long) (extras != null ? extras.get("id") : null);
-			// See if the location is forced to be favorite
-			if (extras != null)
-				isForcedFavorite = extras.getBoolean("forced");
-
-			fetchLocation(locationID);
-		}
-		// If it's a restored instance
-		else {
-			// See if the location is forced to be favorite
-			isForcedFavorite = savedInstanceState.getBoolean("forced");
-
-			// Get the unsaved location from the saved instance
-			location = (CoordinatesLocation) savedInstanceState.getSerializable("location");
-			log.info("Restored saved instance of location: " + location);
-		}
 
 		// Buttons and others
-		saveButton = (Button) findViewById(R.id.title_saveButton);
-		saveButton.setOnClickListener(this);
 		refreshButton = (ImageButton) findViewById(R.id.coordinates_refreshButton);
 		refreshButton.setOnClickListener(this);
 		progressBar = (ProgressBar) findViewById(R.id.coordinates_progressBar);
@@ -126,48 +82,10 @@ public class CoordinatesActivity extends AbstractLocationActivity implements OnC
 		findViewById(R.id.coordinates_radiusSection).setOnClickListener(this);
 
 		// Fill data
-		refreshActivity();
+		refreshLocationView();
+		refreshAbstractLocationView();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see android.app.Activity#onSaveInstanceState(android.os.Bundle)
-	 */
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		outState.putSerializable("location", location);
-		outState.putBoolean("forced", isForcedFavorite);
-	}
-
-	/**
-	 * Refresh the activity displayed views using the data from the location.
-	 */
-	private void refreshActivity() {
-		// Refresh options of the AbstractAlarmLocation
-		refreshAbstractLocationElements();
-
-		// Location Section
-		if (location.isComplete()) {
-			((TextView) findViewById(R.id.coordinates_latText)).setText(nf.format(location.getLatitude()));
-			((TextView) findViewById(R.id.coordinates_longText)).setText(nf.format(location.getLongitude()));
-			if (location.getExtra() != null)
-				((TextView) findViewById(R.id.coordinates_extraText)).setText(location.getExtra());
-			else
-				((TextView) findViewById(R.id.coordinates_extraText)).setText("-");
-		} else {
-			((TextView) findViewById(R.id.coordinates_latText)).setText("Click on the refresh button.");
-			((TextView) findViewById(R.id.coordinates_longText)).setText("-");
-			((TextView) findViewById(R.id.coordinates_extraText)).setText("-");
-		}
-		// Settings section
-		((TextView) findViewById(R.id.coordinates_radiusText)).setText(location.getActivationDistance() + " m");
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see android.app.Activity#onDestroy()
-	 */
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
@@ -177,86 +95,9 @@ public class CoordinatesActivity extends AbstractLocationActivity implements OnC
 		}
 	}
 
-	/**
-	 * Fetches the associated location from the database, or builds a new one, if no id was provided.
-	 * 
-	 * @param locationID the location id
-	 */
-	private void fetchLocation(Long locationID) {
-		if (locationID != null) {
-			log.info("Fetching CoordinatesLocation from database with id: " + locationID);
-			dao.open();
-			location = dao.fetch(locationID);
-			dao.close();
-			log.debug("Fetched location: " + location);
-			if (location != null)
-				return;
-		}
-		// No entity in database... creating a new one
-		log.info("Creating new CoordinatesLocation...");
-		location = new CoordinatesLocation();
-		newEntity = true;
-		if (isForcedFavorite)
-			location.setFavorite(true);
-	}
-
-	/**
-	 * Stores the location in the database.
-	 * 
-	 * @return true, if successful
-	 */
-	private boolean storeLocation() {
-		// Checks
-		if (location == null) {
-			log.error("No location to store in database.");
-			return false;
-		}
-		// Store required data
-
-		// Check if all data is all right
-		if (!location.isComplete()) {
-			log.error("Not all required fields are filled in");
-			return false;
-		}
-
-		// Save the entity to the database
-		log.info("Saving location in database...");
-		dao.open();
-		if (newEntity) {
-			long id = dao.insert(location, true);
-			if (id == -1)
-				return false;
-			log.info("Successfully saved new location with id: " + id);
-			location.setId(id);
-			newEntity = false;
-		} else
-			dao.update(location, location.getId());
-		dao.close();
-
-		return true;
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see android.view.View.OnClickListener#onClick(android.view.View)
-	 */
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
-		case R.id.title_saveButton:
-			log.info("Save button clicked. Storing entity...");
-			if (!storeLocation()) {
-				Toast.makeText(this, "Not all fields are completed corectly. Please check all of them.",
-						Toast.LENGTH_SHORT).show();
-				return;
-			}
-			Intent intent = new Intent();
-			intent.putExtra("id", location.getId());
-			intent.putExtra("type", location.getType().toString());
-			setResult(RESULT_OK, intent);
-			finish();
-			return;
 		case R.id.coordinates_radiusSection:
 			log.debug("Updating radius...");
 			showDialog(DIALOG_ACTIVATION_DISTANCE);
@@ -281,17 +122,9 @@ public class CoordinatesActivity extends AbstractLocationActivity implements OnC
 	// TODO: Cool:
 	// http://stackoverflow.com/questions/7709030/get-gps-location-in-a-broadcast-receiver-or-service-to-broadcast-receiver-data-t/7709140#7709140
 
-	/*
-	 * (non-Javadoc)
-	 * @see android.app.Activity#onCreateDialog(int)
-	 */
 	@Override
 	protected Dialog onCreateDialog(int id) {
 		Dialog dialog;
-		// Check if the AbstractLocationActivity can handle this type of dialog
-		dialog = createAbstractLocationDialog(id);
-		if (dialog != null)
-			return dialog;
 
 		// Try to handle this type of dialog
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -334,8 +167,7 @@ public class CoordinatesActivity extends AbstractLocationActivity implements OnC
 	}
 
 	/*
-	 * Called when a new location is available from the LocationProvider (non-Javadoc)
-	 * @see com.fenceit.provider.CoordinatesLocationDataListener#onLocationUpdate(android.location.Location )
+	 * Called when a new location is available from the LocationProvider.
 	 */
 	@Override
 	public void onLocationUpdate(Location location) {
@@ -349,15 +181,11 @@ public class CoordinatesActivity extends AbstractLocationActivity implements OnC
 				+ new Date(location.getTime()).toString());
 
 		// Update the UI
-		refreshActivity();
+		refreshLocationView();
 		this.progressBar.setVisibility(View.INVISIBLE);
 		this.refreshButton.setVisibility(View.VISIBLE);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see android.app.Activity#onActivityResult(int, int, android.content.Intent)
-	 */
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
@@ -370,12 +198,13 @@ public class CoordinatesActivity extends AbstractLocationActivity implements OnC
 			this.location.setExtra("User selected location");
 
 			// Update the UI
-			refreshActivity();
+			refreshLocationView();
 		}
 	}
 
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see android.app.Activity#onStop()
 	 */
 	@Override
@@ -388,13 +217,49 @@ public class CoordinatesActivity extends AbstractLocationActivity implements OnC
 
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see com.fenceit.ui.AbstractLocationActivity#getLocation()
-	 */
 	@Override
-	protected AbstractAlarmLocation getLocation() {
-		return location;
+	protected DefaultDAO<CoordinatesLocation> getDAO() {
+		// Prepare database connection
+		if (dao == null)
+			dao = DatabaseManager.getDAOInstance(getApplicationContext(), CoordinatesLocation.class,
+					CoordinatesLocation.tableName);
+		return dao;
+	}
+
+	@Override
+	protected CoordinatesLocation instantiateLocation() {
+		return new CoordinatesLocation();
+	}
+
+	@Override
+	protected void refreshLocationView() {
+		// Location Section
+		if (location.isComplete()) {
+			((TextView) findViewById(R.id.coordinates_latText)).setText(nf.format(location.getLatitude()));
+			((TextView) findViewById(R.id.coordinates_longText)).setText(nf.format(location.getLongitude()));
+			if (location.getExtra() != null)
+				((TextView) findViewById(R.id.coordinates_extraText)).setText(location.getExtra());
+			else
+				((TextView) findViewById(R.id.coordinates_extraText)).setText("-");
+		} else {
+			((TextView) findViewById(R.id.coordinates_latText)).setText("Click on the refresh button.");
+			((TextView) findViewById(R.id.coordinates_longText)).setText("-");
+			((TextView) findViewById(R.id.coordinates_extraText)).setText("-");
+		}
+		// Settings section
+		((TextView) findViewById(R.id.coordinates_radiusText)).setText(location.getActivationDistance()
+				+ " m");
+
+	}
+
+	@Override
+	protected void postFetchLocation() {
+		// nothing to do
+	}
+
+	@Override
+	protected void preStoreLocation() {
+		// nothing to do
 	}
 
 }
