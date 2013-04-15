c9f9c896bded63d52d4916d33ba19b352de30b46
Cosmin Stefan <cosminstefanxp@gmail.com>
2013-04-15 13:09:44 +0200

    Updated About to get the version name automatically. Eula not shown every time.

--- fenceit/src/com/fenceit/ui/AboutActivity.java
+++ fenceit/src/com/fenceit/ui/AboutActivity.java
@@ -6,8 +6,11 @@
  */
 package com.fenceit.ui;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.fenceit.R;
 import com.fenceit.ui.helpers.SimpleEula;
@@ -16,6 +19,23 @@ import com.fenceit.ui.helpers.SimpleEula;
  * The Class AboutActivity.
  */
 public class AboutActivity extends DefaultActivity {
+
+	/**
+	 * Gets the package info.
+	 * 
+	 * @return the package info
+	 */
+	private PackageInfo getPackageInfo() {
+		PackageInfo pi = null;
+		try {
+			pi = this.getPackageManager()
+					.getPackageInfo(this.getPackageName(), PackageManager.GET_ACTIVITIES);
+		} catch (PackageManager.NameNotFoundException e) {
+			e.printStackTrace();
+		}
+		return pi;
+	}
+
 	/**
 	 * Called when the activity is first created.
 	 * 
@@ -26,6 +46,8 @@ public class AboutActivity extends DefaultActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about);
 
+		((TextView) this.findViewById(R.id.about_versionText)).setText("Version "
+				+ getPackageInfo().versionName);
 		findViewById(R.id.about_eulaText).setOnClickListener(new View.OnClickListener() {
 
 			@Override
